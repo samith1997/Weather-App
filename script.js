3485030ad33d4ec2404a982c73984c87d10c08e8
Samit <samith415@gmail.com>
2023-03-27 03:28:25 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// apikey="c3db3a6f8c354679b40165348233101"
+
+fetch(http://api.weatherapi.com/v1/current.json?key=c3db3a6f8c354679b40165348233101&q=pakistan&aqi=no
+)
+
+// let weather{
+//     apikey="c3db3a6f8c354679b40165348233101"
+    
+// }
